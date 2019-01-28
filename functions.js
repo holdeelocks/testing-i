95c04c60c7a1082560b6e4b569834f646e8a7e7e
Holden B <holden.bucher.92@gmail.com>
2019-01-28 15:21:27 -0600

    very tedious....

--- functions.js
+++ functions.js
@@ -1,25 +1,57 @@
+function errorCheck(item) {
+	if (!item.lvl || !item.actualName) {
+		throw new Error('item must have enhancement and name properties');
+	}
+
+	if (typeof item.name !== 'string' || typeof item.lvl !== 'number') {
+		throw new Error('name must be string and enhancement must be number');
+	}
+
+	if (item.lvl >= 20 || item.lvl < 0) {
+		return new Error('enhancement can only be between 0 and 20 inclusive');
+	}
+
+	if (0 <= item.level <= 14 && item.durability <= 20) {
+		throw new Error('durability cannot be less than 20 when item enhancement between +0 and +14');
+	}
+
+	if (item.durability < 0 && 15 <= item.lvl <= 20) {
+		throw new Error(
+			'durability cannot be less than zero for item with enhancement between +15 and TET'
+		);
+	}
+
+	// return item;
+}
+
+function increment(item) {
+	const newItem = { ...item };
+	newItem.lvl += 1;
+
+	if (newItem.lvl < 16) {
+		newItem.enhancement = `+${newItem.lvl}`;
+		newItem.name = `[${newItem.enhancement}] ` + newItem.actualName;
+	} else {
+		newItem.enhancement = prefixes[newItem.lvl];
+		newItem.name = `[${newItem.enhancement}] ` + newItem.actualName;
+	}
+	return newItem;
+}
+
+const prefixes = {
+	16: 'PRI',
+	17: 'DUO',
+	18: 'TRI',
+	19: 'TET',
+	20: 'PEN'
+};
+
 module.exports = {
 	success: function(item) {
-		const newItem = { ...item };
-		const prefixes = ['PRI', 'DUO', 'TRI', 'TET', 'PEN'];
-
-		if (!item.enhancement || !item.actualName) {
-			throw new Error('item must have enhancement and name properties');
-		}
-		if (typeof item.name !== 'string' || typeof item.enhancement !== 'number') {
-			throw new Error('name must be string and enhancement must be number');
-		}
-		if (newItem.enhancement >= 20 || newItem.enhancement < 0) {
-			throw new Error('enhancement can only be between 0 and 20 inclusive');
-		}
-
-		newItem.enhancement += 1;
-		if (newItem.enhancment <= 16) {
-			newItem.name = `[${newItem.enhancement}] ` + newItem.actualName;
-		} else {
-			newItem.name = `[${prefixes[newItem.enhancement - 16]}] ` + newItem.actualName;
-		}
-
-		return newItem;
+		errorCheck(item);
+		return increment(item);
+	},
+	repair: function(item) {
+		errorCheck(item);
 	}
 };
--- functions.spec.js
+++ functions.spec.js
@@ -6,7 +6,8 @@ describe('success(item) testing', () => {
 		name: '[DUO] Weenie Hut Junior Sword',
 		type: '__weapon__',
 		durability: 100,
-		enhancement: 17
+		enhancement: 'DUO',
+		lvl: 17
 	};
 
 	const result = enhancer.success(item);
@@ -16,19 +17,22 @@ describe('success(item) testing', () => {
 		name: '[TRI] Weenie Hut Junior Sword',
 		type: '__weapon__',
 		durability: 100,
-		enhancement: 18
+		enhancement: 'TRI',
+		lvl: 18
 	};
 
 	it('should throw error for no name or enhancement', () => {
 		delete item.name;
 		expect(() => enhancer.success(item)).toThrow();
 	});
+
 	it('should error for wrong input types', () => {
 		item.name = 178;
 		expect(() => enhancer.success(item)).toThrow();
 	});
+
 	it('should error for more than 20 or less than 0', () => {
-		item.enhancement = 21;
+		item.lvl = 2345;
 		expect(() => enhancer.success(item)).toThrow();
 	});
 
@@ -37,15 +41,60 @@ describe('success(item) testing', () => {
 		expect(result).not.toBe(item);
 		expect(typeof result).toBe('object');
 	});
+
 	it('should +1 the item enhancement', () => {
 		expect(result.enhancement).toEqual(expected.enhancement);
-		expect(typeof result.enhancement).toBe('number');
+		expect(typeof result.lvl).toBe('number');
+		expect(typeof result.enhancement).toBe('string');
 	});
+
 	it('should change the display name', () => {
 		expect(result.name).toEqual(expected.name);
 	});
+
 	it('should display a prefix not a number', () => {
-		// const name = '[TRI] Weenie Hut Junior Sword';
 		expect(result.name).toEqual(expected.name);
+	});
+});
+
+describe('repair(item) test', () => {
+	const item = {
+		actualName: 'Weenie Hut Junior Sword',
+		name: '[DUO] Weenie Hut Junior Sword',
+		type: '__weapon__',
+		durability: 100,
+		enhancement: 'DUO',
+		lvl: 17
+	};
+
+	const result = enhancer.success(item);
+
+	const expected = {
+		actualName: 'Weenie Hut Junior Sword',
+		name: '[TRI] Weenie Hut Junior Sword',
+		type: '__weapon__',
+		durability: 100,
+		enhancement: 'TRI',
+		lvl: 18
+	};
+
+	it('should throw error for no name or enhancement', () => {
+		delete item.name;
+		expect(() => enhancer.repair(item)).toThrow();
+	});
+
+	it('should error for wrong input types', () => {
+		item.name = 178;
+		expect(() => enhancer.repair(item)).toThrow();
+	});
+
+	it('should error for more than 20 or less than 0', () => {
+		item.lvl = 2345;
+		expect(() => enhancer.repair(item)).toThrow();
+	});
+
+	it('should error for durability lower than allowed', () => {
+		item.durability = -1;
+		expect(() => enhancer.repair(item)).toThrow();
 	});
 });
